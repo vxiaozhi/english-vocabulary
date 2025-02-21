9b4793bbe2a686a85953c098c2ac31cd6500b81d
erikluo <erikluo@tenent.com>
2025-02-21 23:23:26 +0800

    format and stats

---
+++ scripts/proc_format.py
@@ -0,0 +1,35 @@
+import json
+
+with open('json/4-CET6-顺序.json') as f:
+    words = json.load(f)
+
+with open('json/4-CET6-indent.json', 'w') as f:
+    json.dump(words, f, indent=2, ensure_ascii=False)
+
+# 去除words 中的重复单词，保留第一个出现的单词，其余的删除
+# 输出重复词信息。
+words_dict = {}
+repeat_words = []
+for word in words:
+    if word['word'] in words_dict:
+        print("repeat word: ", word['word'])
+        repeat_words.append(word)
+    else:
+        words_dict[word['word']] = word
+
+print("repeat words: ", len(repeat_words))
+words = list(words_dict.values())
+with open('json/4-CET6-indent-no-repeat.json', 'w') as f:
+    json.dump(words, f, indent=2, ensure_ascii=False)
+
+
+# 输出首字母是大写字母的单词
+words_capital = []
+for word in words:
+    if word['word'][0].isupper():
+        words_capital.append(word)
+print("capital words: ", len(words_capital))
+print([word['word'] for word in words_capital])
+        
+
+
